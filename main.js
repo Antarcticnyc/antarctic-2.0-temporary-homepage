2d73c824babe3c7102cd108598d562faf780f9bc
Juan Cortes <juancoob@gmail.com>
2016-04-01 13:20:09 -0400

    first commit landing page 1 image

---
+++ main.js
@@ -0,0 +1,14 @@
+//when mouseenter on .trigger, make the z-index of triger lower than footer and do the translate
+// when mouseleaves footer remove the translate (via class?) and remove the z-index modifier for the trigger
+
+$(document).ready(function(){
+	$('footer').hover(
+		function(){
+		$('.desktop .container').addClass('translated');		
+		},
+		function(){
+		$('.desktop .container').removeClass('translated');
+		console.log('left');
+		}
+	);
+});
